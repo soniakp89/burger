e0a8a1d4c3ea9e56bf04007fbc752136c8c35ffa
soniakp89 <57818045+soniakp89@users.noreply.github.com>
2020-03-31 14:55:08 -0700

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,12 @@
+-- Create database 
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS  burgers
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
